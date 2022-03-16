350bc641a80093f9b23e47d1d0fdb7d0014f36aa
roboticforest <roboticforest@gmail.com>
2022-03-16 00:21:03 -0700

    Hello Behavior
    Added a note about the general behavior of the program and replaced the hello
    world print-out with the most bare minimum of code to replicate the overall
    usage of the final program.

--- main.cpp
+++ main.cpp
@@ -4,8 +4,19 @@
  */
 
 #include <iostream>
+#include <string>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    /*
+     * The original assignment got a compiled program from the user as a single long string of hex pairs. The program
+     * would then be immediately loaded into memory and run. If there was any output from the program it would display
+     * during operation. One thing I don't have any more is Fritz's original VM, so I won't be able to make the
+     * behavior of mine match his. Do I want to though? It doesn't have to be exact.
+     */
+    std::string program;
+
+    std::cout << "LOAD PROGRAM:" << std::endl;
+    std::getline(std::cin, program);
+    std::cout << program;
     return 0;
 }
