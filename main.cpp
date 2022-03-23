d519b0a64202e7b9d02917bc7493595db3436edb
roboticforest <roboticforest@gmail.com>
2022-03-23 16:44:05 -0700

    Basic memory and program input skeleton in place.

--- main.cpp
+++ main.cpp
@@ -25,6 +25,8 @@
 #include <string>
 
 struct Word_12Bit {
+    Word_12Bit() = default;
+
     union {
         unsigned short raw_data: 12;
         struct {
@@ -35,36 +37,86 @@ struct Word_12Bit {
     };
 };
 
+void testWord(){
+    using namespace std;
+    Word_12Bit data;
+    cout << "Raw Word:\t" << data.raw_data << endl; // Uninitialized. Should be a random value.
+    data.raw_data = 5;
+    cout << "Raw Word:\t" << data.raw_data << endl;
+    int num = 0x421; // Dec: 1057
+    data.raw_data = num;
+    cout << "Raw Word:\t" << data.raw_data << endl;
+    cout << "High Nibble:\t" << data.highNibble << endl;
+    cout << "Mid  Nibble:\t" << data.midNibble << endl;
+    cout << "Low  Nibble:\t" << data.lowNibble << endl;
+}
+
 class Memory {
   private:
     Word_12Bit* _memory = nullptr;
     unsigned short _size = 0;
 
   public:
-    Memory(unsigned short size) : _size(size) { _memory = new Word_12Bit[_size]; }
+    Memory(unsigned short size) : _size(size) {
+        _memory = new Word_12Bit[_size];
+        // Initialize all memory to 0.
+        for (auto i = 0; i < _size; ++i) {
+            auto word = _memory[i];
+            word.raw_data = 0;
+        }
+    }
 
-    virtual ~Memory() { delete _memory; }
+    virtual ~Memory() { delete[] _memory; }
 
+    // It doesn't make sense to move or copy memory chips in a running computer.
     Memory(const Memory&) = delete;
     Memory& operator=(const Memory&) = delete;
     Memory& operator=(const Memory&&) = delete;
 
-    Word_12Bit operator[](unsigned short pos) {
-        if (pos < 0 || pos >= _size) {
-            std::cout << "ERROR" << std::endl;
-        }
+    Word_12Bit & operator[](unsigned short pos) {
+        return _memory[pos % _size];
     }
-
 };
 
 int main() {
 
-    std::string program;
+//    testWord();
+//    return 0;
 
     Memory mainMemory(256);
-
+    std::string hexTriple;
+    unsigned int i = 0;
     std::cout << "LOAD PROGRAM:" << std::endl;
-    std::getline(std::cin, program);
-    std::cout << program;
+
+    // BUG: Program terminates with an error status after reading an EOF character from the terminal. It should accept that as an end to the user input for a program.
+    bool validInput = true;
+    while (!std::cin.eof()) {
+        std::cin >> hexTriple;
+        if (!std::cin.good()) {
+            std::cin.clear();  // REMEMBER! You have to clear the error state before you can do ANYTHING with the stream.
+            std::cin.ignore();  // With the error state cleared, you can now ignore the offending character.
+            validInput = false;
+            break;
+        }
+        if (hexTriple.length() != 3) {
+            validInput = false;
+            break;
+        }
+        try {
+            mainMemory[i].raw_data = std::stoi(hexTriple, nullptr, 16);
+        }
+        catch (const std::invalid_argument& exception) {
+            validInput = false;
+            break;
+        }
+        ++i; // TODO: Check if i > mainMemory.size().
+        // TODO: Handle each possible error with an appropriate message for the end user.
+    }
+
+    if (!validInput) {
+        std::cout << "INVALID PROGRAM" << std::endl;
+        return -1;
+    }
     return 0;
 }
+
